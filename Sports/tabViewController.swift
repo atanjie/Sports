89bfd3664089b39b4de9394ee3961de6fba7b71b
gy68456 <806612534@qq.com>
2017-11-09 20:05:22 +0800

    view构建

---
+++ Sports/tabViewController.swift
@@ -0,0 +1,36 @@
+//
+//  tabViewController.swift
+//  Sports
+//
+//  Created by ios20 on 17/11/9.
+//  Copyright © 2017年 ios20. All rights reserved.
+//
+
+import UIKit
+
+class tabViewController: UITabBarController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBOutlet weak var tabbar: UITabBar!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
