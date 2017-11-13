1193726f9308f64723b97d88748d81f97af32dde
gy68456 <806612534@qq.com>
2017-11-13 15:46:50 +0800

    图片

---
+++ Sports/SportsprojectViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SportsprojectViewCell.swift
+//  Sports
+//
+//  Created by ios20 on 17/11/13.
+//  Copyright © 2017年 ios20. All rights reserved.
+//
+
+import UIKit
+
+class SportsprojectViewCell: UICollectionViewCell {
+    @IBOutlet var Sports_Image: UIImageView!
+}
