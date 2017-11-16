b471ed3fcdb1827fbe47451363d055eccd0361f5
gy68456 <806612534@qq.com>
2017-11-16 10:58:23 +0800

    www

--- Sports/DataTableViewController.swift
+++ Sports/DataTableViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class DataTableViewController: UITableViewController {
-    var Array = ["2016-08-05","2016-08-06","2016-08-07","2016-08-08","2016-08-09","2016-08-10","2016-08-11","2016-08-12","2016-08-13","2016-08-14"]
-    var weekday = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+    var schecles : NSArray!
+    var data:[ScheduleModel] = Array<ScheduleModel>();
+    
+    var days = ["2016-08-05","2016-08-06","2016-08-07","2016-08-08","2016-08-09","2016-08-10","2016-08-11","2016-08-12","2016-08-13","2016-08-14"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +30,12 @@ class DataTableViewController: UITableViewController {
     //表格
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return days.count
     }
     //
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return weekday.count
+        return 2
     }
     //表格数
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -47,13 +46,13 @@ class DataTableViewController: UITableViewController {
         var text:String!
         
         for i in 0...10{
-          text = Array[i]
+          text = days[i]
             return text
         }
         return "4"
     }
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return self.Array
+        return self.days
     }
     override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
         return index
@@ -70,19 +69,18 @@ class DataTableViewController: UITableViewController {
         if(cell == nil){//因为是纯代码实现，没有对行里的cell做注册，这里是 做注册， 注册一次后，下次会继续使用这个缓存
             cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: "diy_cell");
         }
-        cell?.textLabel?.text = weekday[indexPath.row]
+        let d = ScheduleDao()
+        var dataday:String!
+        for j in 0...10{
+            dataday = days[j]
+            let s = ScheduleModel(id: 0, eid: 0, gameDate: days[j], gameTime: "", info: "", startGameDate: days[j], endGameDate: "", startGameTime: "", endGameTime: "")
+            let list = d.getSchedule(s)
+            for i in list{
+                cell?.textLabel?.text = i.info
+                return cell!
+            }
+        }
         return cell!
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-
 
 }
